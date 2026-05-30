d077a5b546691f2dd876a729b2506053db5040c1
Ahmed Elawady <medo.sabala@gmail.com>
2026-05-31 04:50:14 +0500

    Add lab6 final task pipeline

---
+++ lab6_final_task/tests/test_api.py
@@ -0,0 +1,28 @@
+﻿from fastapi.testclient import TestClient
+
+from lab6_final_task.app.main import app
+
+
+client = TestClient(app)
+
+
+def test_root_endpoint():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json()["status"] == "ok"
+
+
+def test_predict_endpoint():
+    response = client.post(
+        "/predict",
+        json={
+            "sepal_length": 5.1,
+            "sepal_width": 3.5,
+            "petal_length": 1.4,
+            "petal_width": 0.2
+        }
+    )
+
+    assert response.status_code == 200
+    assert "prediction" in response.json()
+    assert isinstance(response.json()["prediction"], int)
